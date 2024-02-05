0ce68025d8c34f290558ebbba57ac37d9a58aceb
ronaldmaharabam <ronald.maharabam@practo.com>
2024-02-05 14:36:57 +0530

    Initial Commit

---
+++ WebView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebView
+//
+//  Created by Ronald Maharabam on 05/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by Ronald Maharabam on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
